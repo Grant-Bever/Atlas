da4bc385d403fb26444a86c49fb4259ae8074595
cfsarmiento <97933462+cfsarmiento@users.noreply.github.com>
2025-05-05 23:12:02 -0400

    Employee View Semi-Polished
    Visually, it's chillin - just have to make sure functionally that it is sound

--- employee/client/src/components/employee/EmployeeLayout.jsx
+++ employee/client/src/components/employee/EmployeeLayout.jsx
@@ -17,22 +17,11 @@ const EmployeeLayout = () => {
   return (
     <div className="employee-layout">
       <nav className="employee-nav">
-        <TimeTracking />
         <div className="nav-header">
           <h2>Employee Dashboard</h2>
           <p>Welcome, {user.name}</p>
         </div>
-        <ul className="nav-links">
-          <li>
-            <Link to="/employee/timesheet">Timesheet</Link>
-          </li>
-          <li>
-            <Link to="/employee/orders">Active Orders</Link>
-          </li>
-          <li>
-            <Link to="/employee/inventory">Inventory</Link>
-          </li>
-        </ul>
+        <TimeTracking />
       </nav>
       <main className="employee-content">
         <Outlet />
